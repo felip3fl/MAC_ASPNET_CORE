42e1044418e4f4ed2a15fcaba5cd802527362999
ssj4dofuturo <felip3.fl@gmail.com>
2020-06-25 10:38:00 -0300

    Adds .gitignore file

--- MODULO02/mvc_core/mvc_core/Models/Cliente.cs
+++
@@ -1,9 +0,0 @@
-namespace mvc_core.Models
-{
-    public class Cliente
-    {
-        public int ClienteId {get; set;}
-        public string Nome {get; set;}
-        public string Email {get; set;}
-    }
-}
--- MODULO02/mvc_core/mvc_core/Models/Pedido.cs
+++
@@ -1,8 +0,0 @@
-namespace mvc_core.Models
-{
-    public class Pedido
-    {
-        public int PedidoId{get;set;}
-        public string Nome{get;set;}
-    }
-}
