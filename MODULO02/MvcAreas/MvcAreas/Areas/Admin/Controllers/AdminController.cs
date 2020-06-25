990b8f7d2b2474271f6c95ceb26490dd68bc75df
ssj4dofuturo <felip3.fl@gmail.com>
2020-06-25 15:49:18 -0300

    Aula 30 Areas Projeto - Finalizado

---
+++ MODULO02/MvcAreas/MvcAreas/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAreas.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Informacoes()
+        {
+            return View();
+        }
+    }
+}
--- MODULO02/MvcAreas/MvcAreas/Startup.cs
+++ MODULO02/MvcAreas/MvcAreas/Startup.cs
@@ -52,6 +52,9 @@ namespace MvcAreas
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    "areaRouter", "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
