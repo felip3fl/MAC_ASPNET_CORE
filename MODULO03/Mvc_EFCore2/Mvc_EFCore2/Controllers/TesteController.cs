0ff8061dc42b5bf0b34e314572a64e9c57812e48
ssj4dofuturo <felip3.fl@gmail.com>
2020-06-29 10:49:25 -0300

    Aula 22 EFCore Crud Edit

--- MODULO03/Mvc_EFCore2/Mvc_EFCore2/Controllers/TesteController.cs
+++ MODULO03/Mvc_EFCore2/Mvc_EFCore2/Controllers/TesteController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mvc_EFCore2.Models;
 
 namespace Mvc_EFCore2.Controllers
@@ -41,6 +43,64 @@ namespace Mvc_EFCore2.Controllers
 
             return View(aluno);
         }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = _context.Alunos.SingleOrDefault(a => a.Id == id);
+            if(aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,[Bind("Id,Nome,Sexo,Email,Nascimento")]Aluno aluno)
+        {
+            if(id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        private bool AlunoExists(int id) 
+        {
+            return _context.Alunos.Any(e => e.Id == id);
+        }
+
 
     }
 }
