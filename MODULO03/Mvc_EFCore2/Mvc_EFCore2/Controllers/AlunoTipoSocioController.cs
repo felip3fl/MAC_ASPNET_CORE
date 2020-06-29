d4046190592ebba8595edbb07a22cbbd6be816f1
ssj4dofuturo <felip3.fl@gmail.com>
2020-06-29 13:02:35 -0300

    Aula 26 EFCore Exibindo Dados Relacionados

---
+++ MODULO03/Mvc_EFCore2/Mvc_EFCore2/Controllers/AlunoTipoSocioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mvc_EFCore2.Models;
+
+namespace Mvc_EFCore2.Controllers
+{
+    public class AlunoTipoSocioController : Controller
+    {
+        private AlunoContext contexto;
+
+        public AlunoTipoSocioController(AlunoContext _contexto)
+        {
+            contexto = _contexto;
+        }
+
+        public IActionResult Index()
+        {
+            //var infoAluno = contexto.Alunos.ToList();
+            var infoAluno = contexto.Alunos.Include(tp=>tp.TipoSocio);
+            return View(infoAluno);
+        }
+    }
+}
