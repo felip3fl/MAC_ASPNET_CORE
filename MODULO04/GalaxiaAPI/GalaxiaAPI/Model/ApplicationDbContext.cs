790e37636758c42347ede6900129bb954d53b57c
ssj4dofuturo <felip3.fl@gmail.com>
2020-07-02 14:55:57 -0300

    Aula 12 Identity WebAPI - Atualização código

---
+++ MODULO04/GalaxiaAPI/GalaxiaAPI/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GalaxiaAPI.Controllers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalaxiaAPI.Model
+{
+    public class ApplicationDbContext : IdentityDbContext<MeuUserIdentity, MeuRoleIdentity, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- MODULO04/GalaxiaAPI/GalaxiaAPI/Startup.cs
+++ MODULO04/GalaxiaAPI/GalaxiaAPI/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GalaxiaAPI.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +28,13 @@ namespace GalaxiaAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<MeuUserIdentity, MeuRoleIdentity>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +44,8 @@ namespace GalaxiaAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
