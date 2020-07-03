0e245499605dfdd2f5e61329da344ffe6c8096a1
ssj4dofuturo <felip3.fl@gmail.com>
2020-07-03 10:43:56 -0300

    Aula 4 WebAPI Produtos Controller

---
+++ MODULO05/ProdutosWebAPI/ProdutosWebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosWebAPI.Models;
+
+namespace ProdutosWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutosController : Controller
+    {
+        static readonly IProdutoRepositorio repositorio = new ProdutoRepositorio();
+
+        [HttpGet]
+        public IEnumerable<Produto> GetTodos()
+        {
+            return repositorio.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetProduto")]
+        public ActionResult<Produto> GetProdutoPorId(int id)
+        {
+            Produto produto = repositorio.Get(id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(produto);
+        }
+
+        [HttpPost]
+        public ActionResult<Produto> CriaProduto([FromBody] Produto item)
+        {
+            if(item == null)
+                return BadRequest();
+
+            item = repositorio.Add(item);
+            return CreatedAtRoute("GetProduto", new { id = item.Id }, item);
+        }
+
+        [HttpPut]
+        public IActionResult AtualizaProduto(int id, [FromBody] Produto item)
+        {
+            if (item == null)
+                return BadRequest();
+
+            item.Id = id;
+            if (!repositorio.Update(item))
+                return NotFound();
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete]
+        public IActionResult DetetaProduto(int id)
+        {
+            Produto item = repositorio.Get(id);
+            if (item == null)
+                return BadRequest();
+
+            repositorio.Remove(id);
+
+            return new NoContentResult();
+        }
+
+    }
+}
