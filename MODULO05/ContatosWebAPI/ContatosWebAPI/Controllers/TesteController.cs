930970f20b5dc08662a9403bd3f4838ef6d7e23b
ssj4dofuturo <felip3.fl@gmail.com>
2020-07-06 14:24:17 -0300

    Aula 8 WebAPI EF MVC1

---
+++ MODULO05/ContatosWebAPI/ContatosWebAPI/Controllers/TesteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ContatosWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ContatosWebAPI.Controllers
+{
+    public class TesteController : Controller
+    {
+        string BaseUrl = "http://localhost:55631";
+
+        public IActionResult Index()
+        {
+            //criando a lista de contatos
+            List<Contato> contatoLista = new List<Contato>();
+
+            //criando uma instancia de HttpClient
+            using (HttpClient cliente = new HttpClient())
+            {
+                //definindo o endereço base
+                cliente.BaseAddress = new Uri(BaseUrl);
+
+                //definindo o formato dos dados no request
+                MediaTypeWithQualityHeaderValue contentType = new
+                    MediaTypeWithQualityHeaderValue("application/json");
+
+                cliente.DefaultRequestHeaders.Accept.Add(contentType);
+
+                HttpResponseMessage response = cliente.GetAsync("/api/contatos").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var contatoResponse = response.Content.ReadAsStringAsync().Result;
+                    contatoLista = JsonConvert.DeserializeObject<List<Contato>>(contatoResponse);
+                }
+                return View(contatoLista);
+            }
+        }
+    }
+}
