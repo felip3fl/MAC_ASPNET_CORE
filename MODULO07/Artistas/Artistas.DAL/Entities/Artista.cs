cd6383f76611aae8a34bdd1560b7cd399aa59c38
ssj4dofuturo <felip3.fl@gmail.com>
2020-07-06 22:00:14 -0300

    Aula 4 AspNetCore Projeto Camadas1 - Finalizado

---
+++ MODULO07/Artistas/Artistas.DAL/Entities/Artista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Artistas.DAL.Entities
+{
+    public class Artista
+    {
+        public int ArtistaId { get; set; }
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public string Cidade { get; set; }
+        public string Site { get; set; }
+        public DateTime Nascimento { get; set; }
+        public virtual IList<ArtistaDetalhe> ArtistaDetalhes { get; set; }
+    }
+}
